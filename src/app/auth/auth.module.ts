f82b35876b251bfac64a9f954838c2f346dfaf72
Lucas Denipotti <lucasdenipotti@tokenlab.com.br>
2020-11-20 16:07:49 -0300

    Auth Bug Fix

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,12 +1,19 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { SharedModule } from '../shared/shared.module';
 import { AuthRoutingModule } from './auth-routing.module';
 
 import { AuthComponent } from './auth.component';
 
 @NgModule({
-  imports: [CommonModule, ReactiveFormsModule, FormsModule, AuthRoutingModule],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    FormsModule,
+    AuthRoutingModule,
+    SharedModule,
+  ],
   exports: [],
   declarations: [AuthComponent],
   providers: [],
